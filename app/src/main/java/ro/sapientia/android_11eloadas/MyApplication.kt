f8f3e10926bffea88cfed427c5054b9311f706ee
Margit Antal <margitantal68@gmail.com>
2022-11-26 22:35:17 +0200

    third

--- app/src/main/java/ro/sapientia/android_11eloadas/MyApplication.kt
+++ app/src/main/java/ro/sapientia/android_11eloadas/MyApplication.kt
@@ -2,8 +2,6 @@ package ro.sapientia.android_11eloadas
 
 import android.app.Application
 
-// Temporary solution
-// Store the token in SharedPreferences
 class MyApplication : Application() {
     companion object {
         var token: String = ""
